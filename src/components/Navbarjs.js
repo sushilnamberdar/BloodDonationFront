a91d207fe4676772403d42dc5decb4512bce380b
Sushil Namberdar <sushilnamberdar000@gmail.com>
2024-09-17 17:21:23 +0530

    navbar updated

--- src/components/Navbarjs.js
+++ src/components/Navbarjs.js
@@ -57,59 +57,112 @@ const Navbarjs = ({ setToken,setsignup}) => {
   }, [navbarExpanded]);
 
   return (
-    <Navbar
-      expand="lg"
-      className="bg-body-tertiary position-sticky top-0 border h-20 z-20 font-montserrat lg:text-lg"
-      expanded={navbarExpanded}
-      ref={navbarRef}
-    >
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/">
-          <img style={{ height: '40px' }} src={logo} alt="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="navbarScroll"
-          onClick={() => setNavbarExpanded(!navbarExpanded)} // Toggle navbar state
-        />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="ms-auto my-2 my-lg-0 bg-body-tertiary" style={{ maxHeight: '300px' }} navbarScroll>
-            {htoken ? (
-              <>
-                {/* Links to show when htoken is available */}
-                <Nav.Link as={Link} to="/hospitaldashboard">Hospital Dashboard</Nav.Link>
-                <button className='bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600' onClick={handleLogout}>Logout</button>
-              </>
-            ) : atoken ? (
-              <>
-                {/* Links to show when atoken is available */}
-                <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/admin"> Admin Home</Nav.Link>
+<Navbar
+  expand="lg"
+  className={`bg-body-tertiary position-sticky top-0 border z-20 font-montserrat lg:text-lg transition-all duration-300 ease-in-out ${
+    navbarExpanded ? 'h-64' : 'h-20'
+  }`} // Modify the height based on navbarExpanded
+  expanded={navbarExpanded}
+  ref={navbarRef}
+>
+  <Container fluid>
+    <Navbar.Brand as={Link} to="/" className="flex-grow-1">
+      <img style={{ height: '40px' }} src={logo} alt="logo" />
+    </Navbar.Brand>
+    <Navbar.Toggle
+      aria-controls="navbarScroll"
+      onClick={() => setNavbarExpanded(!navbarExpanded)} // Toggle navbar state
+    />
+    <Navbar.Collapse id="navbarScroll" className={navbarExpanded ? "p-0" : "px-4"}>
+      <Nav
+        className={`ms-auto my-2 my-lg-0 bg-body-tertiary w-100 transition-all duration-300 ease-in-out justify-content-end ${
+          navbarExpanded ? 'mt-4' : 'mt-0'
+        }`} // Align items to the right with justify-content-end
+        style={{ maxHeight: '300px' }}
+        navbarScroll
+      >
+        {htoken ? (
+          <>
+            <Nav.Link as={Link} to="/hospitaldashboard" className={navbarExpanded ? 'mt-3' : ''}>Hospital Dashboard</Nav.Link>
+            <button
+              className={`bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </>
+        ) : atoken ? (
+          <>
+            <Nav.Link as={Link} to="/home" className={navbarExpanded ? 'mt-3' : ''}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/admin" className={navbarExpanded ? 'mt-3' : ''}>Admin Home</Nav.Link>
+            <Nav.Link as={Link} to="/manageUsers" className={navbarExpanded ? 'mt-3' : ''}>Manage Users</Nav.Link>
+            <button
+              className={`bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </>
+        ) : token ? (
+          <>
+            <Nav.Link as={Link} to="/bloodRequirement" className={navbarExpanded ? 'mt-3' : ''}>Blood / Camps Request</Nav.Link>
+            <Nav.Link as={Link} to="/home" className={navbarExpanded ? 'mt-3' : ''}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" className={navbarExpanded ? 'mt-3' : ''}>About</Nav.Link>
+            <Nav.Link as={Link} to="/services" className={navbarExpanded ? 'mt-3' : ''}>Services</Nav.Link>
+            <button
+              className={`bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Nav.Link as={Link} to="/" className={navbarExpanded ? 'mt-3' : ''}>Home</Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/hospitalLoginSignup"
+              className={`!bg-indigo-500 px-5 text-white rounded-3xl !hover:bg-indigo-600 mr-1 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+            >
+              Hospital Login
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/loginsignup"
+              className={`!bg-indigo-500 px-5 text-white rounded-3xl !hover:bg-indigo-600 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+              onClick={handellogin}
+            >
+              Login
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/loginsignup"
+              className={`!bg-red-400 px-4 ml-1 text-white rounded-3xl !hover:bg-indigo-600 ${
+                navbarExpanded ? 'mt-3' : ''
+              }`}
+              onClick={handleSignup}
+            >
+              Sign Up
+            </Nav.Link>
+          </>
+        )}
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
-                <Nav.Link as={Link} to="/manageUsers">Manage Users</Nav.Link>
-                <button className='bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600' onClick={handleLogout}>Logout</button>
-              </>
-            ) : token ? (
-              <>
-                {/* Links to show when token is available but htoken and atoken are not */}
-                <Nav.Link as={Link} to="/bloodRequirement">Blood / Camps Request</Nav.Link>
-                <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                <Nav.Link as={Link} to="/services">Services</Nav.Link>
-                <button className='bg-indigo-500 px-5 text-white rounded-3xl hover:bg-indigo-600' onClick={handleLogout}>Logout</button>
-              </>
-            ) : (
-              <>
-                {/* Links to show when neither token, htoken, nor atoken are available */}
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to='/hospitalLoginSignup' className='!bg-indigo-500 px-5 text-white rounded-3xl !hover:bg-indigo-600 mr-1'>Hospital Login</Nav.Link>
-                <Nav.Link as={Link} to="/loginsignup" className='!bg-indigo-500 px-5 text-white rounded-3xl !hover:bg-indigo-600' onClick={handellogin}>Login</Nav.Link>
-                <Nav.Link as={Link} to="/loginsignup" className='!bg-red-400 px-4 ml-1 text-white rounded-3xl !hover:bg-indigo-600' onClick={handleSignup}>Sign Up</Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+
   );
   
 };
